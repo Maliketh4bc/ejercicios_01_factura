4733365e57837443e746afc4590a711eef480a53
Marco Alvarez <malvarm1212@gmail.com>
2025-09-25 13:22:08 +0200

    change: segundo tercio

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        //primer tercio
         System.out.printf("Rojo Polo Paella Inc. \n\n");
         System.out.printf("Carretera Muelle 38 \n");
         System.out.printf("37531 Ávila, Ávila \n\n\n\n");
@@ -8,5 +9,12 @@ public class App {
         System.out.printf("%-30s%-30s%-30s%11s\n", "Virgen BLanca 63", "Cercas Bajas 68", "Nº DE PEDIDO", "1730/2019");
         System.out.printf("%-30s%-30s%-30s%11s\n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "FECHA", "29.01.2019");
         System.out.printf("%-30s%-30s%-30s%11s\n", "", "", "VENCIMIENTO", "");
+        
+        //segundo tercio
+        System.out.println("─────────────────────────────────────────────────────────────────────────────────────────────────────");
+        System.out.printf("%-50s%50s\n", "Total Factura", "199,65");
+        System.out.println("─────────────────────────────────────────────────────────────────────────────────────────────────────");
+
+        //Tercer tercio
     }
 }
