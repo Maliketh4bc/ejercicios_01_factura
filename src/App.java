3a020f155c343c78160bfe3e9db7ba8ffbd024e8
Marco Alvarez <malvarm1212@gmail.com>
2025-09-25 13:09:07 +0200

    change: primer tercio factura

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("Rojo Polo Paella Inc. \n\n");
+        System.out.printf("Carretera Muelle 38 \n");
+        System.out.printf("37531 Ávila, Ávila \n\n\n\n");
+        System.out.printf("%-30s%-30s%-30s%11s\n", "FACTURAR A", "ENVIAR A", "Nº DE FACTURA", "ES-001");
+        System.out.printf("%-30s%-30s%-30s%11s\n", "Leda Villareal", "Leda Villareal", "FECHA", "29.01.2019");
+        System.out.printf("%-30s%-30s%-30s%11s\n", "Virgen BLanca 63", "Cercas Bajas 68", "Nº DE PEDIDO", "1730/2019");
+        System.out.printf("%-30s%-30s%-30s%11s\n", "08759 Burgos, Burgos", "47300 Cádiz, Cádiz", "FECHA", "29.01.2019");
+        System.out.printf("%-30s%-30s%-30s%11s\n", "", "", "VENCIMIENTO", "");
     }
 }
